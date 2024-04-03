884ada3210769b3778b47884e1861e3b997ce864
Javaid Hussain <javaidhussain10@gmail.com>
2024-04-03 23:00:56 +0500

    first commit

---
+++ 9_Favorite Number/9.Favourite-number.js
@@ -0,0 +1,4 @@
+"use strict";
+const favorite_number = 5;
+const message = 'Here is your favoirte number ' + favorite_number;
+console.log(message);
---
+++ 9_Favorite Number/9.Favourite-number.ts
@@ -0,0 +1,5 @@
+const favorite_number : number  = 5 ;
+
+const message : string = 'Here is your favoirte number ' +favorite_number ;
+
+console.log(message) ;
